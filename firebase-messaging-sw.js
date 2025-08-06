6c9710d5ac31333de85ae3b1ced3a78a00027b32
h-social <vinhhien.developer@gmail.com>
2025-08-06 17:39:46 +0700

    Remove TikTok viewer demo and add FCM support
    Deleted TikTok viewer demo files and documentation. Added Firebase Cloud Messaging service worker and integrated FCM notification logic into index.html. Updated manifest app name.

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/12.0.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAXXyKzcIKvKY_STCRHzvaIBn8l5UwGxg8",
+  authDomain: "hsocial-c5ef7.firebaseapp.com",
+  projectId: "hsocial-c5ef7",
+  storageBucket: "hsocial-c5ef7.firebasestorage.app",
+  messagingSenderId: "656660800576",
+  appId: "1:656660800576:web:3dad320a72be9270b26ba5",
+  measurementId: "G-4VZDZPGXSQ"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icons/logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+}); 
